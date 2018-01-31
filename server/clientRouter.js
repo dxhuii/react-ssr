9b4414c14b24f78753b2c1f16dce3505f2be56c9
xiaohui.ding <xiaohui.ding@weimob.com>
2018-01-31 18:43:12 +0800

    优化

--- server/clientRouter.js
+++ server/clientRouter.js
@@ -1,89 +1,82 @@
 import React from 'react';
-import {renderToString} from 'react-dom/server';
-import createHistory from 'history/createMemoryHistory'
+import { renderToString } from 'react-dom/server';
 import { getBundles } from 'react-loadable/webpack';
+import { matchPath } from 'react-router-dom';
+import { matchRoutes } from 'react-router-config';
+import createHistory from 'history/createMemoryHistory';
 import stats from '../dist/react-loadable.json';
 import Helmet from 'react-helmet';
-import {matchPath} from 'react-router-dom';
-import 'isomorphic-fetch'
-import { matchRoutes } from 'react-router-config';
-import client from '../src/app/index.js';
+import client from '../src/app';
 import path from 'path';
-import fs from 'fs'
-let configureStore=client.configureStore;
-let createApp=client.createApp;
-let routesConfig=client.routesConfig;
+import fs from 'fs';
+import 'isomorphic-fetch';
 
-const createStore=(configureStore)=>{
-  let store=configureStore()
+const { configureStore, createApp, routesConfig } = client;
+
+const createStore = (configureStore) => {
+  const store = configureStore();
   return store;
 }
 
-const createTags=(modules)=>{
-  let bundles = getBundles(stats, modules);
-  let scriptfiles = bundles.filter(bundle => bundle.file.endsWith('.js'));
-  let stylefiles = bundles.filter(bundle => bundle.file.endsWith('.css'));
-  let scripts=scriptfiles.map(script=>`<script src="/${script.file}"></script>`).join('\n');
-  let styles=stylefiles.map(style=>`<link href="/${style.file}" rel="stylesheet"/>`).join('\n');
-  return {scripts,styles}
+const createTags = (modules) => {
+  const bundles = getBundles(stats, modules);
+  const scriptfiles = bundles.filter(bundle => bundle.file.endsWith('.js'));
+  const stylefiles = bundles.filter(bundle => bundle.file.endsWith('.css'));
+  const scripts = scriptfiles.map(script => `<script src="/${script.file}"></script>`).join('\n');
+  const styles = stylefiles.map(style => `<link href="/${style.file}" rel="stylesheet"/>`).join('\n');
+  return { scripts, styles };
 }
 
-const prepHTML=(data,{html,head,rootString,scripts,styles,initState})=>{
-  data=data.replace('<html',`<html ${html}`);
-  data=data.replace('</head>',`${head} \n ${styles}</head>`);
-  data=data.replace('<div id="root"></div>',`<div id="root">${rootString}</div>`);
-  data=data.replace('<body>',`<body> \n <script>window.__INITIAL_STATE__ =${JSON.stringify(initState)}</script>`);
-  data=data.replace('</body>',`${scripts}</body>`);
+const prepHTML = (data, { html, head, rootString, scripts, styles, initState }) => {
+  data = data.replace('<html', `<html ${html}`);
+  data = data.replace('</head>', `${head} \n ${styles}</head>`);
+  data = data.replace('<div id="root"></div>', `<div id="root">${rootString}</div>`);
+  data = data.replace('<body>', `<body>\n <script>window.__INITIAL_STATE__ = ${JSON.stringify(initState)}</script>`);
+  data = data.replace('</body>', `${scripts}</body>`);
   return data;
 }
 
-const getMatch=(routesArray, url)=>{
-  return routesArray.some(router=>matchPath(url,{
+const getMatch = (routesArray, url) => {
+  return routesArray.some(router => matchPath(url, {
     path: router.path,
     exact: router.exact,
-  }))
+  }));
 }
 
-const makeup=(ctx,store,createApp,html)=>{
-  let initState=store.getState();
-  let history=createHistory({initialEntries:[ctx.req.url]});
-
-  let modules=[];
-
-  let rootString= renderToString(createApp({store,history,modules}));
-
-  let {scripts,styles}=createTags(modules)
-
-  const helmet=Helmet.renderStatic();
-  let renderedHtml=prepHTML(html,{
+const makeup = (ctx, store, createApp, html) => {
+  const initState = store.getState();
+  const history = createHistory({ initialEntries: [ctx.req.url] });
+  const modules = [];
+  const rootString = renderToString(createApp({ store, history, modules }));
+  const { scripts, styles } = createTags(modules);
+  const helmet = Helmet.renderStatic();
+  const renderedHtml = prepHTML(html, {
     html:helmet.htmlAttributes.toString(),
     head:helmet.title.toString()+helmet.meta.toString()+helmet.link.toString(),
     rootString,
     scripts,
     styles,
-    initState
+    initState,
   })
   return renderedHtml;
 }
 
 
-const clientRouter=async(ctx,next)=>{
-  let html=fs.readFileSync(path.join(path.resolve(__dirname,'../dist'),'index.html'),'utf-8');
-  let store=createStore(configureStore);
-
-  let branch=matchRoutes(routesConfig,ctx.req.url)
-  let promises = branch.map(({route,match})=>{
-    return route.thunk?(route.thunk(store)):Promise.resolve(null)
+const clientRouter = async(ctx, next) => {
+  const html = fs.readFileSync(path.join(path.resolve(__dirname, '../dist'), 'index.html'), 'utf-8');
+  const store = createStore(configureStore);
+  const branch = matchRoutes(routesConfig, ctx.req.url);
+  const promises = branch.map(({ route, match })=>{
+    return route.thunk ? (route.thunk(store)) : Promise.resolve(null)
   });
-  await Promise.all(promises).catch(err=>console.log('err:---',err))
+  await Promise.all(promises).catch(err => console.log('err:---', err))
 
-  let isMatch=getMatch(routesConfig,ctx.req.url);
+  const isMatch = getMatch(routesConfig, ctx.req.url);
   if(isMatch){
-    let renderedHtml=await makeup(ctx,store,createApp,html);
-    ctx.body=renderedHtml
+    const renderedHtml = await makeup(ctx, store, createApp, html);
+    ctx.body = renderedHtml;
   }
-  await next()
+  await next();
 }
 
 export default clientRouter;
-
--- src/index.js
+++ src/index.js
@@ -1,46 +1,45 @@
 import React from 'react';
-import {hydrate,render} from 'react-dom';
+import { hydrate, render } from 'react-dom';
 import createHistory from 'history/createBrowserHistory'
 import Loadable from 'react-loadable';
 import NProgress from 'nprogress';
 import './assets/css/nprogress.scss';
-import app from './app/index.js';
+import app from './app';
 
 const initialState = window && window.__INITIAL_STATE__;
-let history = createHistory()
-let {configureStore,createApp}=app;
-let store=configureStore(initialState)
+const history = createHistory();
+const { configureStore, createApp } = app;
+const store = configureStore(initialState);
 
-const renderApp=()=>{
-  let application=createApp({store,history});
+const renderApp = () => {
+  const application = createApp({ store, history });
   const renderMethed = process.env.NODE_ENV === 'development' && module.hot ? render : hydrate
-  renderMethed(application,document.getElementById('root'));
+  renderMethed(application, document.getElementById('root'));
 }
 
 window.main = () => {
-  NProgress.start()
+  NProgress.start();
   Loadable.preloadReady().then(() => {
-    renderApp()
-    NProgress.done()
+    renderApp();
+    NProgress.done();
   });
 };
 
-
-if(process.env.NODE_ENV==='development'){
+if(process.env.NODE_ENV === 'development'){
   if(module.hot){
-    module.hot.accept('./store/reducers/index.js',()=>{
-      let newReducer=require('./store/reducers/index.js');
+    module.hot.accept('./store/reducers/index.js', () => {
+      const newReducer = require('./store/reducers/index.js');
       store.replaceReducer(newReducer)
       /*import('./store/reducers/index.js').then(({default:module})=>{
         store.replaceReducer(module)
       })*/
     })
-    module.hot.accept('./app/index.js',()=>{
-      let {createApp}=require('./app/index.js');
-      let newReducer=require('./store/reducers/index.js');
-      store.replaceReducer(newReducer)
-      let application=createApp({store,history});
-      hydrate(application,document.getElementById('root'));
+    module.hot.accept('./app/index.js', () => {
+      const { createApp } = require('./app/index.js');
+      const newReducer = require('./store/reducers/index.js');
+      store.replaceReducer(newReducer);
+      const application = createApp({ store, history });
+      hydrate(application, document.getElementById('root'));
       /*import('./app/index.js').then(({default:module})=>{
         let {createApp}=module;
         import('./store/reducers/index.js').then(({default:module})=>{
@@ -52,6 +51,3 @@ if(process.env.NODE_ENV==='development'){
     })
   }
 }
-
-
-
